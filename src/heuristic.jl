f723f2ef1d328ce8a99fabc64775010ecf5e47ca
Maxime Mouchet <max@maxmouchet.com>
2020-05-17 16:20:47 +0200

    Add HeuristicCache

--- src/heuristic.jl
+++ src/heuristic.jl
@@ -1,6 +1,18 @@
 # https://hal.laas.fr/hal-02413636/document -- Heuristic 2
 
-function surrogate_mdp(mdp::MonitoringMDP, s::State{P}, a::Action{P}, i::Int)::MonitoringMDP{2} where {P}
+HeuristicCache = Dict{Tuple{Int,Float64}, ValueIterationPolicy}
+
+struct HeuristicPolicy{P} <: Policy
+    mdp::MonitoringMDP{P}
+    solver::Solver
+    cache::HeuristicCache
+end
+
+function HeuristicPolicy(mdp, solver)
+    HeuristicPolicy(mdp, solver, HeuristicCache())
+end
+
+function expected_delay(mdp::MonitoringMDP, s::State{P}, a::Action{P}, i::Int) where {P}
     indices = vcat(1:i-1, i+1:P)
 
     # > First, path i is excluded in the computation of the mean delay which depends upon
@@ -9,32 +21,39 @@ function surrogate_mdp(mdp::MonitoringMDP, s::State{P}, a::Action{P}, i::Int)::M
     # HACK: build an MDP excluding path `i` and with no monitoring costs
     others::MonitoringMDP{P-1} = MonitoringMDP(mdp.models[indices], mdp.τmax[indices], zeros(length(indices)), mdp.discount)
     dist = transition(others, s[indices], a[indices])
-    expected_delay = sum(weighted_iterator(dist)) do (sp, p)
+
+    sum(weighted_iterator(dist)) do (sp, p)
         p * delay(others, sp)
     end
-
-    p1 = mdp.models[i] # Stochastic path
-    p2 = HMM(ones(1,1), [Constant(expected_delay)]) # Deterministic path
-
-    MonitoringMDP([p1, p2], [mdp.τmax[i], 0], [mdp.costs[i], 0], mdp.discount)
 end
 
-function heuristic_step(solver::Solver, mdp::MonitoringMDP, s::State{P}, a::Action{P}, i::Int) where {P}
-    surrogate = surrogate_mdp(mdp, s, a, i)
-    policy = solve(solver, surrogate)
+function heuristic_step(policy::HeuristicPolicy, s::State{P}, a::Action{P}, i::Int) where {P}
+    delay = expected_delay(policy.mdp, s, a, i)
+    key = (i, delay)
+
+    policy = get!(policy.cache, key) do
+        # Surrogate MDP
+        # p1: stochastic path
+        # p2: virtual path
+        p1 = policy.mdp.models[i]
+        p2 = HMM(ones(1,1), [Constant(delay)])
+        surrogate = MonitoringMDP(
+            [p1, p2],
+            [policy.mdp.τmax[i], 0],
+            [policy.mdp.costs[i], 0],
+            policy.mdp.discount
+        )
+        solve(policy.solver, surrogate)
+    end
+
     action(policy, (s[i], DiscreteBelief(0,1)))[1]
 end
 
-struct HeuristicPolicy{P} <: Policy
-    mdp::MonitoringMDP{P}
-    solver::Solver
-end
 
-# Algorithm 2
 function action(policy::HeuristicPolicy{P}, s::State{P}) where{P}
     a = zeros(Bool, P)
     for i = 1:P
-        a[i] = heuristic_step(policy.solver, policy.mdp, s, Tuple(a), i)
+        a[i] = heuristic_step(policy, s, Tuple(a), i)
     end
     Tuple(a)
 end
